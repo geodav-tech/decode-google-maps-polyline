465e9fd59cefe2eff11c5b13db35d8ada7c4c92a
Mike Davlantes <rocks722@hotmail.com>
2015-11-06 15:11:03 -0700

    Added function to convert steps (as returned from the Maps API) to shapefile
    Feed this your Maps API response and it'll return you a shapefile;
    requires arcpy.

--- polyline_decoder.py
+++ polyline_decoder.py
@@ -1,4 +1,6 @@
+# This function is free of any dependencies.
 def decode_polyline(polyline_str):
+    '''Pass a Google Maps encoded polyline string; returns list of lat/lon pairs'''
     index, lat, lng = 0, 0, 0
     coordinates = []
     changes = {'latitude': 0, 'longitude': 0}
@@ -30,3 +32,30 @@ def decode_polyline(polyline_str):
         coordinates.append((lat / 100000.0, lng / 100000.0))
 
     return coordinates
+
+# This function requires Esri's arcpy module.
+def convert_to_shapefile(steps, output_shapefile):
+    '''Pass the steps object returned by the Maps API (should be response['routes'][0]['legs'][0]['steps'])
+    and an output shapefile path; outputs a detailed shapefile of that route'''
+    
+    import arcpy, os
+
+    # Decode each step of the route; add those coordinate pairs to a list
+    total_route = []
+    for step in steps:
+        total_route += decode_polyline(step['polyline']['points'])
+
+    # Create empty WGS84 shapefile.
+    sr = arcpy.SpatialReference(4326)
+    arcpy.CreateFeatureclass_management(os.path.dirname(output_shapefile), os.path.basename(output_shapefile), 
+        "POLYLINE", spatial_reference=sr)
+
+    # Add points to array, write array to shapefile as a polyline
+    arr = arcpy.Array()
+    for coord_pair in total_route:
+        arr.add(arcpy.Point(coord_pair[1], coord_pair[0]))
+    with arcpy.da.InsertCursor(output_shapefile, ['SHAPE@']) as rows:
+        rows.insertRow([arcpy.Polyline(arr)])
+    del rows
+
+    return output_shapefile
